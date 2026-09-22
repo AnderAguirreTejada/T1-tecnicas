4d98ecba14903c1240ba2912fcb84d4dbef67227
Frank <n00404486@upn.pe>
2026-09-21 19:38:34 -0500

    Agregados geters y setters

--- Trabajo-T1/src/trabajo/t1/Alumno.java
+++ Trabajo-T1/src/trabajo/t1/Alumno.java
@@ -15,5 +15,44 @@ public class Alumno {
     private String nivel_economico;
     private String tipo_beca;
 
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }-tipo_beca
 
+    public void setTipo_doc(String tipo_doc) {
+        this.tipo_doc = tipo_doc;
+    }
+
+    public void setNum_doc(String num_doc) {
+        this.num_doc = num_doc;
+    }
+
+    public void setNivel_economico(String nivel_economico) {
+        this.nivel_economico = nivel_economico;
+    }
+
+    public void setTipo_beca(String tipo_beca) {
+        this.tipo_beca = tipo_beca;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getTipo_doc() {
+        return tipo_doc;
+    }
+
+    public String getNum_doc() {
+        return num_doc;
+    }
+
+    public String getNivel_economico() {
+        return nivel_economico;
+    }
+
+    public String getTipo_beca() {
+        return tipo_beca;
+    }
+    
 }
