4ace240dea87c43f88bc1a877884bcb097e2bf1a
Frank <n00404486@upn.pe>
2026-09-21 19:38:06 -0500

    Clase creada Alumno

---
+++ Trabajo-T1/src/trabajo/t1/Alumno.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package trabajo.t1;
+
+/**
+ *
+ * @author UCA40408
+ */
+public class Alumno {
+    private String nombre;
+    private String tipo_doc;
+    private String num_doc;
+    private String nivel_economico;
+    private String tipo_beca;
+
+
+}
